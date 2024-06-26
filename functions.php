5143fd07cc4570f22c8552c5485161a0699bfcd4
Mathieu Lamiot <mathieu.lamiot@free.fr>
2024-06-26 22:03:29 +0200

    Define wpm_apply_filters_custom_guard to support custom safeguard logic

--- functions.php
+++ functions.php
@@ -32,6 +32,26 @@ function wpm_apply_filters_typed( $type, $hook_name, $value, ...$args ) {
 }
 
 /**
+ * Calls the callback functions that have been added to a filter hook and ensures
+ * the returned value passes the custom guard filters.
+ *
+ * @param string $custom_guard_name The type the return value should have.
+ * @param string $hook_name 		The name of the filter hook.
+ * @param mixed  $value     		The value to filter.
+ * @param mixed  ...$args   		Additional parameters to pass to the callback functions.
+ * @return mixed The filtered value after all hooked functions are applied to it.
+ */
+function wpm_apply_filters_custom_guard( $custom_guard_name, $hook_name, $value, ...$args ) {
+	$next_value = apply_filters( $hook_name, $value, ...$args );
+	$custom_guard_hook = 'wpm_filter_guard_' . $custom_guard_name;
+	if ( ! wpm_apply_filters_typesafe($custom_guard_hook, true, $next_value, ...$args ) ) {
+		return $value;
+	}
+
+	return $next_value;
+}
+
+/**
  * Checks whether the given variable is a certain type.
  *
  * Returns whether `$value` is certain type.
