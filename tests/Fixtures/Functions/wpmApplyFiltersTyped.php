d8f5bca83e85196d1f01d92283b63ef6f886ac4d
Rémy Perona <remy@wp-media.me>
2024-09-16 08:21:47 -0400

    Closes #8 Add support for typed arrays (#12)

--- tests/Fixtures/Functions/wpmApplyFiltersTyped.php
+++ tests/Fixtures/Functions/wpmApplyFiltersTyped.php
@@ -50,4 +50,51 @@ return [
 		'warning' => false,
 		'expected' => null,
 	],
+	'testShouldReturnOriginalWhenArrayOfStringsNotArray' => [
+		'type' => 'string[]',
+		'value' => [
+			'string1',
+			'string2',
+		],
+		'filter_return' => 0,
+		'warning' => true,
+		'expected' => [
+			'string1',
+			'string2',
+		],
+	],
+	'testShouldReturnOriginalWhenArrayOfStringsNotMatched' => [
+		'type' => 'string[]',
+		'value' => [
+			'string1',
+			'string2',
+		],
+		'filter_return' => [
+			'string1',
+			0,
+		],
+		'warning' => true,
+		'expected' => [
+			'string1',
+			'string2',
+		],
+	],
+	'testShouldReturnArrayOfStringsWhenArrayOfStrings' => [
+		'type' => 'string[]',
+		'value' => [
+			'string1',
+			'string2',
+		],
+		'filter_return' => [
+			'string1',
+			'string2',
+			'string3',
+		],
+		'warning' => false,
+		'expected' => [
+			'string1',
+			'string2',
+			'string3',
+		],
+	],
 ];
